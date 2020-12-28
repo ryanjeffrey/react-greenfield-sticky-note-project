4e0f45414de9f08c741c5a50d1644c7d06038c84
ryanjeffrey <73365865+ryanjeffrey@users.noreply.github.com>
2020-12-28 14:55:11 -0600

    Made sticky note text editable

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ for this application, delegating rendering logic to
 presentational components. */
 class App extends Component {
   state = {
-    notes: [
-      {
-        id: Date.now(),
-        title: "",
-        description: "",
-        doesMatchSearch: true
-      }
-    ],
+    notes: [],
     searchText: ""
   };
   addNote = () => {
@@ -27,11 +20,36 @@ class App extends Component {
     var newNotes = [newNote, ...this.state.notes];
     this.setState({ notes: newNotes });
   };
+  onType = (editMeId, updatedKey, updatedValue) => {
+    /* this event handler updates sticky note text fields
+      - editMeId: the id of the note that the user typed in
+      - updatedKey: which field was edited? 'title' or 'description'
+      - updatedValue: new value of edited field */
+    var updateIdMatch = (note) => {
+      if (note.id !== editMeId) {
+        return note;
+      } else {
+        if (updatedKey === "title") {
+          return {
+            ...note,
+            title: updatedValue
+          };
+        } else {
+          return {
+            ...note,
+            description: updatedValue
+          };
+        }
+      }
+    };
+    var updatedNotes = this.state.notes.map(updateIdMatch);
+    this.setState({ notes: updatedNotes });
+  };
   render() {
     return (
       <div>
         <Header searchText={this.state.searchText} addNote={this.addNote} />
-        <NotesList notes={this.state.notes} />
+        <NotesList notes={this.state.notes} onType={this.onType} />
       </div>
     );
   }
--- src/Note.js
+++ src/Note.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 
-var Note = (props) => (
-  <li className="note">
-    <input
-      type="text"
-      placeholder="Title"
-      className="note__title"
-      value={props.note.title}
-    />
-    <textarea
-      placeholder="Description..."
-      className="note__description"
-      value={props.note.description}
-    />
-    <span className="note__delete">X</span>
-  </li>
-);
+/* This presentational component renders a single
+list item element with two text input fields,
+one for the note's title and one for the note's
+description. When the user types in these fields,
+functions that are passed to this component
+as props are used to update the state of the
+corresponding note object in the state of the
+App component  */
+class Note extends Component {
+  updateTitle = (e) => {
+    var updatedValue = e.target.value;
+    var editMeId = this.props.note.id;
+    this.props.onType(editMeId, "title", updatedValue);
+  };
+  updateDescription = (e) => {
+    var updatedValue = e.target.value;
+    var editMeId = this.props.note.id;
+    this.props.onType(editMeId, "description", updatedValue);
+  };
+  render() {
+    return (
+      <li className="note">
+        <input
+          type="text"
+          placeholder="Title"
+          className="note__title"
+          value={this.props.note.title}
+          onChange={this.updateTitle}
+        />
+        <textarea
+          placeholder="Description..."
+          className="note__description"
+          value={this.props.note.description}
+          onChange={this.updateDescription}
+        />
+        <span className="note__delete">X</span>
+      </li>
+    );
+  }
+}
 
 export default Note;
