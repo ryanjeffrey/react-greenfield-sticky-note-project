8ef535e9e9427347d6f2e251564984088e4a1a1f
ryanjeffrey <73365865+ryanjeffrey@users.noreply.github.com>
2020-12-28 12:12:41 -0600

    Passed props down the component tree

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <NotesList />
+        <Header searchText={this.state.searchText} />
+        <NotesList notes={this.state.notes} />
       </div>
     );
   }
--- src/NotesList.js
+++ src/NotesList.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Note from "./Note.js";
 
-var NotesList = () => (
-  <div>
-    <Note />
-    <Note />
-    <Note />
-  </div>
-);
+const renderNote = (note) => <Note note={note} key={note.id} />;
+
+var NotesList = (props) => {
+  var noteElements = props.notes.map(renderNote);
+  return <ul className="notes-list">{noteElements}</ul>;
+};
 
 export default NotesList;
