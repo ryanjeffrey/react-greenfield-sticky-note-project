ed8dbccd48db36f53a5ad02cca4defb238b86e07
ryanjeffrey <73365865+ryanjeffrey@users.noreply.github.com>
2020-12-28 14:18:44 -0600

    Added functionality to "+ New Note" button

--- src/App.js
+++ src/App.js
@@ -9,30 +9,28 @@ class App extends Component {
   state = {
     notes: [
       {
-        id: 0,
-        title: "eat",
-        description: "reese peanut butter cups",
-        doesMatchSearch: true
-      },
-      {
-        id: 1,
-        title: "sleep",
-        description: "eight hours",
-        doesMatchSearch: true
-      },
-      {
-        id: 2,
-        title: "code",
-        description: "build an awesome ui",
+        id: Date.now(),
+        title: "",
+        description: "",
         doesMatchSearch: true
       }
     ],
-    searchText: "search for me"
+    searchText: ""
+  };
+  addNote = () => {
+    var newNote = {
+      id: Date.now(),
+      title: "",
+      description: "",
+      doesMatchSearch: true
+    };
+    var newNotes = [newNote, ...this.state.notes];
+    this.setState({ notes: newNotes });
   };
   render() {
     return (
       <div>
-        <Header searchText={this.state.searchText} />
+        <Header searchText={this.state.searchText} addNote={this.addNote} />
         <NotesList notes={this.state.notes} />
       </div>
     );
