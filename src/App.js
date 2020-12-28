ad3600903197265ad75f49a943c398a334c3adbf
ryanjeffrey <73365865+ryanjeffrey@users.noreply.github.com>
2020-12-28 11:26:52 -0600

    Created modules for each component and rendered components in App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import React from "react";
+import Header from "./Header";
+import NotesList from "./NotesList";
 
 var App = () => (
   <div>
-    <header className="app-header">
-      <h1 className="app-header__title">Sticky Icky Notes</h1>
-      <aside className="app-header__controls">
-        <button className="add-new">+ New Note</button>
-        <input
-          type="text"
-          placeholder="Type here to search..."
-          className="search"
-        />
-      </aside>
-    </header>
-    <ul className="notes-list">
-      <li className="note">
-        <input type="text" placeholder="Title" className="note__title" />
-        <textarea placeholder="Description..." className="note__description" />
-        <span className="note__delete">X</span>
-      </li>
-      <li className="note">
-        <input type="text" placeholder="Title" className="note__title" />
-        <textarea placeholder="Description..." className="note__description" />
-        <span className="note__delete">X</span>
-      </li>
-      <li className="note">
-        <input type="text" placeholder="Title" className="note__title" />
-        <textarea placeholder="Description..." className="note__description" />
-        <span className="note__delete">X</span>
-      </li>
-    </ul>
+    <Header />
+    <NotesList />
   </div>
 );
 
---
+++ src/NotesList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Note from "./Note.js";
+
+var NotesList = () => (
+  <div>
+    <Note />
+    <Note />
+    <Note />
+  </div>
+);
+
+export default NotesList;
